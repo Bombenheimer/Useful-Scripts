a654d79c92b85f4e47f59f5710c562aaa2644b30
Bombenheimer <145699702+Bombenheimer@users.noreply.github.com>
2024-04-02 04:48:14 -0400

    Update delete_Garbage.sh

--- delete_Garbage.sh
+++ delete_Garbage.sh
@@ -3,7 +3,7 @@
 shred_Garbage()
 {
 	local file
-	find /Users/bombenheimer/Documents/Garbage -type f 2>/dev/null | while read -r file; do
+	find /home/bombenheimer/.Garbage -type f 2>/dev/null | while read -r file; do
 		shred -ufvz -n 3 "$file"
 	done
 }
